a7efd24323c9282b8a01dc30b4d004ac29ae03c9
Vaenthan Jeevarajah <vaenthan.jeevarajah@gem.com>
2022-07-19 15:05:56 -0700

    pls work

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -19,7 +19,7 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: "TESING CI AGAIN",
+  label: "TESING CI",
 };
 
 export const Secondary = Template.bind({});
