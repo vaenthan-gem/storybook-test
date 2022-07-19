920aedb86a8747833b1dd2a205e0bcbd53b17107
Vaenthan Jeevarajah <vaenthan.jeevarajah@gem.com>
2022-07-19 15:50:33 -0700

    trying

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -19,7 +19,7 @@ export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: "Okay trying something new",
+  label: "sdlkfjfbndfsljkfn",
 };
 
 export const Secondary = Template.bind({});
